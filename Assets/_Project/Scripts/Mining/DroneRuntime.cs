f3af674c261380136abafd8fb9893059b5ba531d
Christian Valari <christian@valarisolutions.com>
2026-07-03 11:50:53 +0800

    mining: drop DroneRuntime cargo API, replaced by direct per-asteroid payout

--- Assets/_Project/Scripts/Mining/DroneRuntime.cs
+++ Assets/_Project/Scripts/Mining/DroneRuntime.cs
@@ -5,28 +5,11 @@ namespace SocialUniverse.Mining
 {
     public class DroneRuntime
     {
-        public DroneDefinition Definition  { get; }
-        public int             CargoAmount { get; private set; }
-        public bool            IsCargoFull => CargoAmount >= Definition.CargoCap;
+        public DroneDefinition Definition { get; }
 
         public DroneRuntime(DroneDefinition definition)
         {
             Definition = definition ?? throw new ArgumentNullException(nameof(definition));
-        }
-
-        public int AddCargo(int amount)
-        {
-            int space  = Definition.CargoCap - CargoAmount;
-            int actual = Math.Min(amount, space);
-            CargoAmount += actual;
-            return actual;
-        }
-
-        public int EmptyCargo()
-        {
-            int hauled  = CargoAmount;
-            CargoAmount = 0;
-            return hauled;
         }
     }
 }
