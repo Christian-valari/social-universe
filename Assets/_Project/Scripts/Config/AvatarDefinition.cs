b9f7bc41f4d595e214020067f27f82b089a4c895
Christian Valari <christian@valarisolutions.com>
2026-07-06 17:36:47 +0800

    config: add AvatarDefinition ScriptableObject for the avatar catalog

---
+++ Assets/_Project/Scripts/Config/AvatarDefinition.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SocialUniverse.Config
+{
+    [CreateAssetMenu(menuName = "SocialUniverse/Config/AvatarDefinition", fileName = "NewAvatar")]
+    public class AvatarDefinition : ScriptableObject
+    {
+        [SerializeField] private string _avatarId;
+        [SerializeField] private Sprite _sprite;
+
+        public string AvatarId => _avatarId;
+        public Sprite Sprite   => _sprite;
+    }
+}
